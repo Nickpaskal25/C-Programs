d6d5ebb1dc36215c17ea17eb9d33b84df1e9f334
Nickpaskal25 <nicholasotieno@zetech.ac.ke>
2026-05-25 20:15:57 +0300

    Add files via upload

---
+++ Scholarship_awards.cpp
@@ -0,0 +1,44 @@
+//Name: Nicholas Otieno
+//ADM BCS-05-0046/2026
+//University Scholarship Awards
+
+#include<iostream>
+using namespace std;
+int main()
+    {
+        //User enters Student Name, Exam marks, attendance percentage
+        string name;
+        int marks, attendance;
+    cout<<"\tUniversity Scholarship Awards\n" <<endl;
+    cout<<"Enter Student Name, Exam marks, attendance percentage respectively: \n" <<endl;
+    cin>>name >>marks >> attendance;
+
+        //scholarship eligibility score >=70( attendance>=80 above) score>=50(>=85) below 50
+        //full scholarship, Partial scholarship
+    string scholarship_status;
+    if (marks>=70)
+        {
+            if (attendance>=80){scholarship_status="Full Scholarship";}
+
+            else{scholarship_status="Partial Scholarship";}
+        }
+    else if (marks>=60)
+        {
+            if (attendance>=85){scholarship_status="Full Scholarship";}
+
+            else{scholarship_status="Partial Scholarship";}
+        }
+    else
+        {
+            scholarship_status="No Scholarship";
+        }
+
+
+        //student name and scholarship status
+    cout<<"Student Name: "<<""<<name<<"\n"
+        <<"Student Marks: "<<marks<<"\n"
+        <<"Attendance Percentage: "<<attendance<<"%\n"
+        <<"Scholarship Status: "<<scholarship_status<<"\n"   ;
+
+    return 0;
+    }
