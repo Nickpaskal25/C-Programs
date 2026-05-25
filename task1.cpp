3cda296644d4b37c5f116ef686e5a4f39aa0e539
Nickpaskal25 <nicholasotieno@zetech.ac.ke>
2026-05-25 13:29:10 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,45 @@
+//task 1 calculate book overdue,charge fine.
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"\tLIBRARY CHARGES/BOOK OVERDUE FINE";
+
+    int BookId, Due_Date, Return_Date ;
+
+    //1.input from user(Book ID, Due Date, Return Date ) all ints
+    cout<<"\nEnter BookId, Due_Date, Return_Date respectively."
+          "\n Press enter or spacebar after every input \n";
+    cin>>BookId >> Due_Date >> Return_Date;
+
+    //2 calculate overdue= ReturnDate - DueDate
+    int Over_Due = Return_Date-Due_Date;
+
+    //3 if else for fine rates: Fine rates <=7 20, <=14 50, >=15 100.
+    int Fine_Rate;
+    if (Over_Due<=0)
+        {
+            Fine_Rate=00;
+        }
+    else if (Over_Due <=7 )
+        {
+         Fine_Rate=20;
+        }
+    else if ((Over_Due<=14))
+        {
+         Fine_Rate=50;
+        }
+    else if ((Over_Due>=15))
+        {
+         Fine_Rate=100;
+        };
+
+    //4 Output (BookID, DueDate, ReturnDate, DaysOverdue, FineRate,FineAmount)
+    cout<<"\n\t\t !!FINAL OUTPUT!!";
+    cout<<"\n You Entered: "
+    <<"\n\tBookId: "<<BookId
+    <<"\n\tDue_Date: "<< Due_Date
+    <<"\n\tReturn_Date: "  << Return_Date
+    <<"\n\tDays Overdue: "<<Over_Due<<"days"
+    <<"\n\tFine_Rate: Ksh."<<Fine_Rate;
+    return 0;
+}
