2f9916aa58cc5a3c5bb9516e5ba394a33a2bef80
Nickpaskal25 <nicholasotieno@zetech.ac.ke>
2026-06-16 09:04:34 +0300

    Update and rename Password_Verification_System_do_while_loop.py to Password_Verification_System_do_while_loop.cpp

---
+++ Password_Verification_System_do_while_loop.cpp
@@ -0,0 +1,47 @@
+///Name : Nicholas Otieno
+//ADM : BCS-05-0045/2025
+//Week 4 Activity: Loops
+
+/*
+Task 3: Password Verification System (do-while loop)
+An online system requires users to enter correct login credentials before accessing
+services. Write a C++ program that performs the following tasks:
+i. Stores a predefined username and password.
+ii. Uses a do-while loop to repeatedly prompt the user to enter
+username and password.
+iii. Checks the input:
+? If correct ? Display ?Access Granted? and exit the
+loop
+? If incorrect ? Display ?Incorrect credentials, try
+again?
+iv. The system should continue until correct credentials are entered.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+ cout <<"\t Password Verification System" <<endl;
+ string User_Name, Pass ;
+
+ int trials = 0;
+
+ do {
+  cout <<"Username: ";
+  cin >>User_Name;
+
+  cout << "Password: ";
+  cin >> Pass;
+  if (User_Name == "NickOti" and Pass == "Nick45?" ) {
+   cout <<"\n\t Access Granted" <<endl;
+  break;
+  }
+
+  else {cout <<"\n\tIncorrect credentials, try again" <<endl;}
+
+  trials++;
+ } while (trials >= 0);
+
+  return 0;
+ }
