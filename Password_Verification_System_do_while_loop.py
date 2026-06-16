d905ba26409cd2e6827b552790306ee7f9ec211d
Nickpaskal25 <nicholasotieno@zetech.ac.ke>
2026-06-16 08:18:11 +0300

    Add files via upload

---
+++ Password_Verification_System_do_while_loop.py
@@ -0,0 +1,13 @@
+"""
+Task 3: Password Verification System (do-while loop)
+An online system requires users to enter correct login credentials before accessing
+services. Write a C++ program that performs the following tasks:
+i. Stores a predefined username and password.
+ii. Uses a do-while loop to repeatedly prompt the user to enter
+username and password.iii. Checks the input:
+• If correct → Display “Access Granted” and exit the
+loop
+• If incorrect → Display “Incorrect credentials, try
+again”
+iv. The system should continue until correct credentials are entered.
+"""
---
+++ Savings_Withdrawal_System_while_loop.py
@@ -0,0 +1,15 @@
+"""
+Task 2: Savings Withdrawal System (while loop)
+A mobile money agent wants to create a simple system that allows customers to withdraw
+money repeatedly until they choose to stop. Write a C++ program that performs the
+following tasks:
+i. Declares a variable to store account balance and withdrawal amount.
+ii. Uses a while loop to allow repeated withdrawals as long as the balance is
+sufficient.
+iii. After each withdrawal:
+• Deducts the amount from the balance
+• Displays the remaining balance
+iv. The loop should stop when:
+• The withdrawal amount is greater than the balance, OR
+• The balance becomes zero
+"""
